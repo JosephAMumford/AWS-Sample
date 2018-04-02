44b26caea8f6fb68e36c9e011276a66d8c049247
JosephAMumford <JosephAMumford@gmail.com>
2018-04-02 14:34:59 -0500

    update

--- apps/main/models.py
+++ apps/main/models.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Quote(models.Model):
+    text = models.TextField()
+    season = models.IntegerField()
+    episode = models.IntegerField()
+    character = models.ForeignKey(Character, related_name='quotes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class DatabaseStats(models.Model):
+    count = models.IntegerField()
+    records = models.IntegerField()
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
